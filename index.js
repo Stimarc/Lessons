724d27b97c52d5ed59fff3f7d6c8c7b653b6404e
Stimarc <rikudo7474@gmail.net>
2023-04-04 22:24:17 +0300

    update 04.04 22:24

---
+++ index.js
@@ -0,0 +1,18 @@
+// var name = "Alex";
+
+// let name_1 = "Dmytro";
+
+// let name_2 = "Lizzy";
+
+let role = prompt("хто ви?")
+switch (person) {
+    case "admin":
+        console.log('admin');
+        break;
+    case "user":
+        console.log('user');
+        break;
+    default:
+        console.log('?????');
+        break;
+}
